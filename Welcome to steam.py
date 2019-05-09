4bcfd313fc304717d5d808e3a2e0937de91e26f6
Nina Lyn <50455665+ninalyn2323@users.noreply.github.com>
2019-05-09 13:25:38 -0400

    Update Welcome to steam.py

--- Welcome to steam.py
+++ Welcome to steam.py
@@ -1,4 +1,24 @@
 from sense_hat import SenseHat
-sense + SEnseHat()
+sense = SEnseHat()
 
 sense.show_message("Welcome to Joe Walker STEAM Night!")
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+y = (255, 255, 0)
+b = (0, 0, 0)
+
+crescent_moon = [ 
+    b, b, y, y, y, y, b, b,
+    b, y, y, y, b, b, b, b,
+    y, y, y, b, b, b, b, b, 
+    y, y, b, b, b, b, b, b,
+    y, y, b, b, b, b, b, b,
+    y, y, y, b, b, b, b, b,
+    b, y, y, y, b, b, b, b,
+    b, b, y, y, y, y, b, b,
+]
+
+    sense.set_pixels(crescent_moon)
